a7112701c94f281af4e48dbf9179bc2dbeabb732
Anton Karpov <slam.caponezed@mail.ru>
2025-02-14 19:22:31 +0300

    add image to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
-  templateUrl: './header.component.html',
+  template: `
+    <div class="header">
+      <div class="d-flex align-items-center gap-1">
+        <img class="logo" src="logo.svg">
+        <h4 class="m-0 fw-bold">{{ appTitle }}</h4>
+      </div>
+    </div>
+  `,
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  @Input({ required: true }) appTitle: string = 'Shopic';
 }
