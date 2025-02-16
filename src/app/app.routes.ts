2cc81078ab97feb3be2bcacef6bd0869fcab58a0
Anton Karpov <slam.caponezed@mail.ru>
2025-02-16 17:46:50 +0300

    add product page component and register its route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeContainer } from './pages/home/containers/home/home.container';
 import { CatalogContainer } from './pages/catalog/containers/catalog/catalog.container';
+import { ProductContainer } from './pages/product/containers/product/product.container';
 
 export const routes: Routes = [
   { path: 'home', component: HomeContainer },
   { path: 'catalog', component: CatalogContainer },
+  { path: 'product/:id', component: ProductContainer },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
---
+++ src/app/pages/product/containers/product/product.container.ts
@@ -0,0 +1,47 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
+import { ProductsService } from '../../../../services/products.service';
+import { Product } from '../../../../models/product.model';
+
+@Component({
+  selector: 'app-product',
+  imports: [CurrencyPipe],
+  templateUrl: './product.container.html',
+  styleUrl: './product.container.css'
+})
+export class ProductContainer implements OnInit {
+  productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+
+  product: Product = {
+    id: 0,
+    name: 'Товар 1',
+    description: 'Описание 1',
+    imgSrc: 'image-placeholder.jpg',
+    productType: {
+      name: "Тип 1"
+    },
+    price: 0,
+  };
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const productId = params['id'];
+
+      this.getProductById(productId);
+    });
+  }
+
+  getProductById(productId: number) {
+    this.productsService.getProductById(productId)
+      .pipe(
+        take(1)
+      )
+      .subscribe(product => {
+        this.product = product;
+      });
+  }
+
+}
