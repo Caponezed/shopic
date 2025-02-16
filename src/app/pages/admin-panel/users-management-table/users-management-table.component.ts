a4ca3994cd73b076fd4d63f1a5e0bb8b99f719e9
Anton Karpov <slam.caponezed@mail.ru>
2025-02-16 22:15:41 +0300

    init users management table component

---
+++ src/app/pages/admin-panel/users-management-table/users-management-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-management-table',
+  imports: [],
+  templateUrl: './users-management-table.component.html',
+  styleUrl: './users-management-table.component.css'
+})
+export class UsersManagementTableComponent {
+
+}
