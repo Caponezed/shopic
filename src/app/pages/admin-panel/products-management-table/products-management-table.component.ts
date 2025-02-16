3f0e17e7ad1a60f53baeddb72364b5a0a63b89fc
Anton Karpov <slam.caponezed@mail.ru>
2025-02-16 22:16:13 +0300

    add products management table component

---
+++ src/app/pages/admin-panel/products-management-table/products-management-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../models/product.model';
+
+@Component({
+  selector: 'app-products-management-table',
+  imports: [],
+  templateUrl: './products-management-table.component.html',
+  styleUrl: './products-management-table.component.css'
+})
+export class ProductsManagementTableComponent {
+  @Input() products: Product[] = [];
+
+  @Output() openAddNewProductDialogEmitter = new EventEmitter<void>();
+  @Output() openUpdateProductDialogEmitter = new EventEmitter<Product>();
+  @Output() deleteProductByIdEmitter = new EventEmitter<Product>();
+
+  openAddNewProductDialog() {
+    this.openAddNewProductDialogEmitter.emit();
+  }
+
+  deleteProductById(product: Product) {
+    this.deleteProductByIdEmitter.emit(product);
+  }
+
+  openUpdateProductDialog(product: Product) {
+    this.openUpdateProductDialogEmitter.emit(product);
+  }
+}
