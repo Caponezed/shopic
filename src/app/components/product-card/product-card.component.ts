1099947279d4221d207e1c322fb677f1d01761da
Anton Karpov <slam.caponezed@mail.ru>
2025-02-17 11:51:45 +0300

    enable angular src directive for product-card image and restrict height

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { ShopicButtonDirective } from '../../directives/shopicButton/shopic-button.directive';
 import { Product } from '../../models/product.model';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
-  imports: [ShopicButtonDirective, CurrencyPipe, RouterLink],
+  imports: [ShopicButtonDirective, CurrencyPipe, RouterLink, NgOptimizedImage],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
