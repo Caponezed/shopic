00877c13a7c35ee65f33afafdfbbd98287a8debc
Anton Karpov <slam.caponezed@mail.ru>
2025-02-17 09:09:09 +0300

    add base_url global variable and use it in products service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, InjectionToken, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+export const BASE_URL = new InjectionToken<string>('BASE_URL');
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: BASE_URL, useValue: 'http://localhost:8080' }
   ]
 };
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,31 +1,31 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
+import { BASE_URL } from '../app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private readonly httpClient = inject(HttpClient);
-  private readonly apiUrl = 'http://localhost:8080/api';
+  constructor(@Inject(BASE_URL) private baseUrl: string, private readonly httpClient: HttpClient) { }
 
   getAllProducts() {
-    return this.httpClient.get<Product[]>(`${this.apiUrl}/products`);
+    return this.httpClient.get<Product[]>(`${this.baseUrl}/api/products`);
   }
 
   getProductById(productId: number) {
-    return this.httpClient.get<Product>(`${this.apiUrl}/products/${productId}`);
+    return this.httpClient.get<Product>(`${this.baseUrl}/api/products/${productId}`);
   }
 
   addNewProduct(product: Product) {
-    return this.httpClient.post<Product>(`${this.apiUrl}/products`, product);
+    return this.httpClient.post<Product>(`${this.baseUrl}/api/products`, product);
   }
 
   updateProduct(product: Product) {
-    return this.httpClient.put<Product>(`${this.apiUrl}/products`, product);
+    return this.httpClient.put<Product>(`${this.baseUrl}/api/products`, product);
   }
 
   deleteProductById(productId: number) {
-    return this.httpClient.delete<void>(`${this.apiUrl}/products/${productId}`);
+    return this.httpClient.delete<void>(`${this.baseUrl}/api/products/${productId}`);
   }
 }
