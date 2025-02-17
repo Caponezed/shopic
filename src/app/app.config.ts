e6aedf8a8125eb247c6f1e467d00cc15d8512704
Anton Karpov <slam.caponezed@mail.ru>
2025-02-17 12:44:33 +0300

    add new global files endpoint variable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,14 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const BASE_URL = new InjectionToken<string>('BASE_URL');
+export const FILES_API_ENDPOINT = new InjectionToken<string>('FILES_API_ENDPOINT');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    { provide: BASE_URL, useValue: 'http://localhost:8080' }
+    { provide: BASE_URL, useValue: 'http://localhost:8080' },
+    { provide: FILES_API_ENDPOINT, useValue: 'http://localhost:8080/api/files/' },
   ]
 };
