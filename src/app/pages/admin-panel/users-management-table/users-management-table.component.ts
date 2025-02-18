625ec1a3a652a59881e40288d81d4e3343590dea
Anton Karpov <slam.caponezed@mail.ru>
2025-02-18 15:10:39 +0300

    create users table component according to product table component

--- src/app/pages/admin-panel/users-management-table/users-management-table.component.ts
+++ src/app/pages/admin-panel/users-management-table/users-management-table.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../../models/user.model';
+import { JoinPipe } from '../../../pipes/join.pipe';
 
 @Component({
   selector: 'app-users-management-table',
-  imports: [],
+  imports: [JoinPipe],
   templateUrl: './users-management-table.component.html',
   styleUrl: './users-management-table.component.css'
 })
 export class UsersManagementTableComponent {
+  @Input({ required: true }) users: User[] = [];
 
+  @Output() openAddNewUserDialogEmitter = new EventEmitter<void>();
+  @Output() openUpdateUserDialogEmitter = new EventEmitter<User>();
+  @Output() deleteUserByIdEmitter = new EventEmitter<User>();
+
+  openAddNewUserDialog() {
+    this.openAddNewUserDialogEmitter.emit();
+  }
+  openUpdateUserDialog(user: User) {
+    this.openUpdateUserDialogEmitter.emit(user);
+  }
+  deleteUserById(user: User) {
+    this.deleteUserByIdEmitter.emit(user);
+  }
 }
