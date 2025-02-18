702079909c449100a047db9f66491387f888d930
Anton Karpov <slam.caponezed@mail.ru>
2025-02-18 15:03:45 +0300

    add new role and user models

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+import { Role } from "./role.model";
+
+export interface User {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  roles: Role[];
+}
