be35f00422f9169363359a05950e588ba3399ac9
Anton Karpov <slam.caponezed@mail.ru>
2025-02-19 10:29:20 +0300

    register login route and add jwt token key name of local storage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,21 @@ import { ApplicationConfig, InjectionToken, provideZoneChangeDetection } from '@
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './interceptors/error.interceptor';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const BASE_URL = new InjectionToken<string>('BASE_URL');
 export const FILES_API_ENDPOINT = new InjectionToken<string>('FILES_API_ENDPOINT');
+export const JWT_KEY_NAME = new InjectionToken<string>('SHOPIC_JWT_KEY');;
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     { provide: BASE_URL, useValue: 'http://localhost:8080' },
     { provide: FILES_API_ENDPOINT, useValue: 'http://localhost:8080/api/files/' },
+    { provide: JWT_KEY_NAME, useValue: "SHOPIC_JWT_KEY" },
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { HomeContainer } from './pages/home/containers/home/home.container';
 import { CatalogContainer } from './pages/catalog/containers/catalog/catalog.container';
 import { ProductContainer } from './pages/product/containers/product/product.container';
 import { AdminPanelContainer } from './pages/admin-panel/containers/admin-panel/admin-panel.container';
+import { LoginContainer } from './pages/login/login.container';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginContainer },
   { path: 'home', component: HomeContainer },
   { path: 'catalog', component: CatalogContainer },
   { path: 'product/:id', component: ProductContainer },
